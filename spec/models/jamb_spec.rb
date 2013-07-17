73eda181155794695b7cbf1ab6c4cd852c4895f2
Bratislav Petrovic <bratislav.petrovic@gmail.com>
2013-07-17 18:29:41 +0200

    more tests

--- spec/models/jamb_spec.rb
+++ spec/models/jamb_spec.rb
@@ -1,5 +1,5 @@
 describe Jamb do
-  it "must be crated" do
+  it "must be created" do
     j = Jamb.new
   end
 
@@ -13,6 +13,7 @@ describe Jamb do
     j = Jamb.new
     j.columns do |col|
       assert_equal :LABEL, j.cell(0,col).type
+      assert_equal false, j.cell(0,col).is_enabled?
     end
   end
   
@@ -23,4 +24,26 @@ describe Jamb do
       assert_equal false, j.cell(row,0).is_enabled?
     end
   end
+  
+  it "rows from 1 to 6 and 9 and 10 must be normal if col <> 0 " do
+    j = Jamb.new
+    j.rows do |row|
+      j.columns do |col|
+        if [1,2,3,4,5,6,9,10].include?(row) && col != 0
+          assert_equal :NORMAL, j.cell(row,col).type
+        end
+     end
+    end
+  end
+  it "rows from 7 and 8 must be calc and disabled if col <> 0 " do
+    j = Jamb.new
+    j.rows do |row|
+      j.columns do |col|
+        if [7,8].include?(row) && col != 0
+          assert_equal :CALC, j.cell(row,col).type
+        end
+     end
+    end
+  end
+
 end
