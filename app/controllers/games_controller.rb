5b63ac7dd693efad76bdbde7e86f20a2db2fc404
Bratislav Petrovic <bratislav.petrovic@gmail.com>
2013-07-27 19:04:16 +0200

    working link

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,10 @@
 class GamesController < ApplicationController
   def show
     @jamb = Jamb.new
-  end   
+  end
+   
   def play
     @jamb = Jamb.new
+    @jamb.set_cell_value(params[:row].to_i,params[:col].to_i,3)
   end  
 end
