780de4346f8d83529c6b5c8f033c5582581126a9
Bratislav Petrovic <bratislav.petrovic@gmail.com>
2013-08-22 07:10:12 +0200

    all games stats

--- app/models/games_stat.rb
+++ app/models/games_stat.rb
@@ -1,4 +1,16 @@
 class GamesStat
+  def self.average_best_score
+    a = ActiveRecord::Base.connection.select_all("select AVG(CAST(value as integer) ) from user_stats where property = 'BEST_SCORE'")
+    a[0]['avg'].to_i
+  end
+  def self.all_user_average
+    a = ActiveRecord::Base.connection.select_all("select AVG(CAST(value as float) ) from user_stats where property = 'AVERAGE'")
+    a[0]['avg'].to_f
+  end
+  def self.games_played
+    a = ActiveRecord::Base.connection.select_all("select SUM(CAST(value as float) ) from user_stats where property = 'GAMES_PLAYED'")
+    a[0]['sum'].to_f
+  end
   def self.best_scores
     a = Array.new  
     best = UserStat.where(property: "BEST_SCORE").order("value desc").first(3)
